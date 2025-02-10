5ad76b7dff74679c55331dfc5e1afb6f583e829e
Manuel <mhummel@student.42heilbronn.de>
2025-02-10 09:45:26 +0100

    Create main.cpp
    Made the main as in the subject

---
+++ ex00/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhummel <mhummel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/10 09:40:53 by mhummel           #+#    #+#             */
+/*   Updated: 2025/02/10 09:43:55 by mhummel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Fixed.hpp"
+
+int	main(void)
+{
+	Fixed a;
+	Fixed b(a);
+	Fixed c;
+
+	c = b;
+
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+
+	return (0);
+}
